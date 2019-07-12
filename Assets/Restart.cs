3377b25074eff7007ef6551ec227f783788fffc7
David Teller <D.O.Teller@gmail.com>
2019-07-12 21:57:55 +0200

    Initial commit

---
+++ Assets/Restart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restart : MonoBehaviour
+{
+    public void Update() {
+        if (UnityEngine.Input.GetMouseButton(0) || UnityEngine.Input.touchCount > 0) {
+            this.Reroll();
+        }
+    }
+
+    public void Reroll() {
+        Debug.Log("Rerolling !");
+        foreach (string id in new List<string> {"Fate Die 1", "Fate Die 2", "Fate Die 3", "Fate Die 4"}) {
+            GameObject.Find(id).SendMessage("Reroll");
+        }
+    }
+}
---
+++ Assets/Roll.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roll : MonoBehaviour
+{
+    System.Random rand = new System.Random();
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        Rigidbody body = this.GetComponent<Rigidbody>();
+
+        // Add a velocity, just to make it nice.
+        body.velocity = new Vector3(
+            -rand.Next(20),
+            0,
+            rand.Next(10) - 5
+        );
+
+        // Add a torque, to actually randomize the results.
+        body.AddTorque(
+            rand.Next(1000),
+            rand.Next(1000),
+            rand.Next(1000)
+        );
+    }
+
+    void Reroll()
+    {
+        Rigidbody body = this.GetComponent<Rigidbody>();
+
+        // Bounce up the die
+        body.velocity = new Vector3(
+            rand.Next(11) - 5,
+            rand.Next(5) + 1,
+            rand.Next(11) - 5
+        );
+
+        // Add a torque, to actually randomize the results.
+        body.AddTorque(
+            rand.Next(1000),
+            rand.Next(1000),
+            rand.Next(1000)
+        );
+    }
+}
